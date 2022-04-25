4b0d4907b454d26618c167d3cb1e66c053485c15
jtba <jasonthomasbaldwin@gmail.com>
2022-04-24 18:47:49 -0700

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "DEVersity";
+
+// render(what to see, where to see)
 ReactDOM.render(
   <div>
-    <h1>Hello World</h1>
+    <h1>Hello {name}</h1>
+    {
+      // Use curly braces to jump out of JSX
+    }
+    <p>Your lucky number is: {getNumber()} </p>
   </div>,
   document.getElementById("root")
 );
+
+function getNumber() {
+  if (name === "DEVersity") {
+    return Math.floor(Math.random() * 100);
+  }
+}
